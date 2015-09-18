c0bc4cdf87b7370dbba05b1ecdc91a1f50df0b2b
fmdcarvalho <filipe.m.carvalho@gmail.com>
2015-09-18 17:37:44 +0000

    added gravity towards cursor

--- particles.js
+++ particles.js
@@ -2,6 +2,22 @@ var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 var focus = true;
 var fps = 1000/60;
+var mousePos = {'x':-1 , 'y':-1};
+
+canvas.width = window.innerWidth; 
+canvas.height = window.innerHeight; 
+//tamanho canvas
+var W = canvas.width; 
+var H = canvas.height;
+var maxVelocity = 6;
+var x = 100; var y = 100;
+var bgImage =document.getElementById('bgImage'); 
+
+var particles = [];
+var particleGroup = [];
+
+var noGroups = 0;
+
 
 window.onblur = function (event) {
     fps = 1000/500; 
@@ -15,6 +31,22 @@ window.onresize = function(event) {
 	resizeCanvas();
 }
 
+function getMousePos(canvas, evt) {
+	var rect = canvas.getBoundingClientRect();
+	return {
+		x: evt.clientX - rect.left,
+		y: evt.clientY - rect.top
+	};
+}
+
+canvas.addEventListener('mousemove', function(evt) {
+	mousePos = getMousePos(canvas, evt);
+});
+
+canvas.addEventListener('mouseout', function(evt) {
+	mousePos = {'x':-1 , 'y':-1};
+});
+
 function resizeCanvas() { 
 	console.log("resize");
 	canvas.width = window.innerWidth; 
@@ -22,27 +54,14 @@ function resizeCanvas() {
 //	init();
 }
 
-canvas.width = window.innerWidth; 
-canvas.height = window.innerHeight; 
-//tamanho canvas
-var W = canvas.width; 
-var H = canvas.height;
-
-var x = 100; var y = 100;
-var bgImage =document.getElementById('bgImage'); 
-
-var particles = [];
-var particleGroup = [];
-
-var noGroups = 0;
-
 function init(){
 
 	W = canvas.width; 
 	H = canvas.height;
 	particles = [];
 	particleGroup = [];
-	noGroups = ((canvas.width + canvas.height)*0.01)>>0
+//	noGroups = ((canvas.width + canvas.height)*0.01)>>0
+	noGroups = 1;
 	for(var i = 0; i < 0; i++)
 	{
 		particles.push(new create_particle());
@@ -67,7 +86,8 @@ function create_group(){
 	seedParticle.vx = Math.random()*2-1;
 	seedParticle.vy = Math.random()*2-1;
 	
-	for ( var i = 0 ; i <= noParticles ; i ++ ){
+	for ( var i = 0 ; i <= 1; i ++ ){
+	//for ( var i = 0 ; i <= noParticles ; i ++ ){
 
 		var point = generateRandomPoint(seedParticle, Math.random() * 5 + 3);
 		var particle = create_particle_from_point(point.x , point.y, seedParticle.vx, seedParticle.vy);
@@ -139,6 +159,25 @@ function manhatanDistance(point1 , point2)
 	return Math.abs(point1.x - point2.x) + Math.abs(point1.y - point2.y);
 }
 
+function calculateDesiredVelocity(point){
+	var relVelocity = ( maxVelocity) / (manhatanDistance(point, mousePos));
+	var relativeMaxVelocity = relVelocity > maxVelocity ? maxVelocity : relVelocity;
+	if(relativeMaxVelocity>1)console.log( relativeMaxVelocity);
+	var norm = normalize({'x' : mousePos.x - point.x, 'y': mousePos.y - point.y});
+	return {
+		'vx' : norm.x * relativeMaxVelocity,
+		'vy' : norm.y * relativeMaxVelocity
+	}
+}
+
+function normalize(v){
+	length = Math.sqrt(v.x * v.x + v.y * v.y);
+	return { 
+		'x' : v.x /= length,
+		'y' : v.y /= length
+	}
+}
+
 function draw()
 {
 	ctx.globalCompositeOperation = "source-over";
@@ -187,10 +226,15 @@ function draw()
 
 		ctx.strokeStyle = "rgba("+r+", "+g+", "+b+", 0.5)"
 		ctx.stroke();
-	
+		
+		if(mousePos.x != -1){
+			var desiredVelocity = calculateDesiredVelocity(p);	
+			p.vx = p.vx + desiredVelocity.vx > maxVelocity ? (p.vx < 0 ? maxVelocity * -1 : maxVelocity) : p.vx + desiredVelocity.vx;
+			p.vy = p.vy + desiredVelocity.vy > maxVelocity ? (p.vy < 0 ? maxVelocity * -1 : maxVelocity) : p.vy + desiredVelocity.vy;
+
+		}
 		p.x += p.vx;
 		p.y += p.vy;
-		
 		if(p.x < -50) p.vx = p.vx*-1;
 		if(p.y < -50) p.vy = p.vy*-1;
 		if(p.x > W+50) p.vx = p.vx*-1;
